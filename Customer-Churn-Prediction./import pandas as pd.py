2c809ba167edbfc8a819ebc83a77ded8fb25d1a8
smibrahimali <ialiibra2@gmail.com>
2026-08-05 07:11:42 +0500

    Initial Commit

--- Customer-Churn-Prediction./import pandas as pd.py
+++ Customer-Churn-Prediction./import pandas as pd.py
@@ -2,17 +2,13 @@ import pandas as pd
 
 df = pd.read_csv("dataset.csv")
 
-# df.head()
-# df.info()
-
-# print(df[df['TotalCharges'].str.strip() == ''])
-
 print(df[df['TotalCharges'].str.strip() == ''][['customerID', 'tenure', 'TotalCharges']])
 
 df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
 df['TotalCharges'] = df['TotalCharges'].fillna(0)
 print(df['TotalCharges'].isnull().sum())
+df = df.drop("customerID",axis =1 )
+df['Churn'] = df['Churn'].map({'Yes':1, 'No': 0})
 
-df = df.drop('customerID', axis=1)
-
-df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
+print(df.head())
+print(df.info())
